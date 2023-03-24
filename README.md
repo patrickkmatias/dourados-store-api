e88615155d422ab572d9facee8cb94ccfab580a5
Patrick <99044724+pMattheew@users.noreply.github.com>
2023-03-24 17:36:02 -0300

    feat: e2e testing w/ pactum (#9)

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # dourados-store-api
 nestjs api designed to attend the client app dourados-store | admissional test for "Play For a Cause"
+
+--- 
+
+#### Ports explained: 
+
+- 3000 | Development server
+- 3300 | E2E test application
+- 5432 | Development database
+- 7777 | Testing database
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,54 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { Product } from '@prisma/client';
 import { AppModule } from './../src/app.module';
+import { eachLike } from 'pactum-matchers';
+import { spec, request } from 'pactum';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+      }),
+    );
+
     await app.init();
+    await app.listen(3300);
+    request.setBaseUrl('http://localhost:3300');
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/ (GET) | should redirect to /products', async () => {
+    return await spec().get('/').expectStatus(301);
+  });
+
+  it('/products (GET) | should get all products', async () => {
+    const mockProd: Product = {
+      id: 0,
+      name: 'Shiver',
+      slug: 'shiver',
+      description: 'shiver',
+      price: 0,
+      price_id: 'price...',
+      imageUrl: 'image.com',
+    };
+
+    return await spec()
+      .get('/products')
+      .expectStatus(200)
+      .expectJson({
+        products: eachLike(mockProd),
+      });
   });
 });
