b6c376ee80737ae663453b08e691bc9bebc96001
Patrick <99044724+pMattheew@users.noreply.github.com>
2023-03-27 00:50:43 -0300

    feat: initial vercel config (#15)

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+    allowedHeaders: '*',
+    optionsSuccessStatus: 200,
+  });
 }
 bootstrap();
